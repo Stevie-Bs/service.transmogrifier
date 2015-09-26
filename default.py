cf003bd76e94e54749d6fd4c9290d14501c141fa
LarrySellers <LittleLarrySellers@runbox.com>
2015-09-26 18:24:41 -0400

    modified:   default.py 	new file:   resources/www/html/images/movie_pending.png 	new file:   resources/www/html/images/tvshow_pending.png

--- default.py
+++ default.py
@@ -37,24 +37,68 @@ def view_queue():
 		def set_info_controls(self):
 			label = self.create_label("File:")
 			self.add_label(label, 0, 0, pad_x=15, pad_y=10)
-			label = self.create_label("Status:")
+			
+			self.filename = self.create_label("")
+			self.add_label(self.filename, 0, 1, pad_x=15, pad_y=10)
+			
+			label = self.create_label("Status:")			
 			self.add_label(label, 1, 0, pad_x=15, pad_y=10)
+			
+			self.status = self.create_label("")
+			self.add_label(self.status, 1, 1, pad_x=15, pad_y=10)
+			
 			label = self.create_label("Progress:")
 			self.add_label(label, 2, 0, pad_x=15, pad_y=10)
 			
 			label = self.create_label("Size:")
 			self.add_label(label, 0, 3, pad_x=15, pad_y=10)
 			
+			self.size = self.create_label("")
+			self.add_label(self.size, 0, 4, pad_x=15, pad_y=10)
+			
 			label = self.create_label("Priority:")
 			self.add_label(label, 1, 3, pad_x=15, pad_y=10)
 			
-			self.create_progress_bar('progress')
-			self.add_object('progress', 2,1,columnspan=4, pad_y=14)
+			self.priority = self.create_label("")
+			self.add_label(self.priority, 1, 4, pad_x=15, pad_y=10)
 			
-			items = ["file 1", "file 2"]
+			self.create_progress_bar('progress')
+			self.add_object('progress', 2, 1, columnspan=4, pad_y=14)
+			
+			queue = [
+					["tvshow 1", "tvshow", "tvshow1.avi", 1, 42, '512 mb'],
+					["tvshow 2", "tvshow", "tvshow2.avi", 0, 0, '546 mb'],
+					["tvshow 3", "tvshow", "tvshow3.avi", 0, 0, '546 mb'],
+					["movie 1", "movie", "movie1.avi", 0, 0, '1516 mb'],
+			]
+			
+			def show_status():
+				print "ok"
+				obj = self.getFocus()
+				index = int(obj.getSelectedItem().getProperty('index'))
+				self.priority.setLabel(str(index+1))
+				self.filename.setLabel(queue[index][2])
+				self.size.setLabel(queue[index][5])
+				if queue[index][3] == 1:
+					status = 'active'
+					percent = float(queue[index][4]) / 100
+					self.update_progress_bar('progress', percent, '1.4 MB/s')
+				else:
+					self.update_progress_bar('progress', 0, '')
+					status = 'pending'
+				self.status.setLabel(status)	
+				
+				
+			
+			items = [item[0] for item in queue]
 			self.create_list('queue')
-			self.add_object('queue', 3,0,7,5)
-			self.add_list_items('queue', items, 0, allow_multiple=False, allow_toggle=False)
+			self.add_object('queue', 3,1,7,4)
+			self.add_list_items('queue', items, selectable=False, call_back=show_status)
+			icon_root = vfs.join(ROOT_PATH, 'resources/www/html/images')
+			for item in queue:
+				index = queue.index(item)
+				icon = vfs.join(icon_root, item[1] + '.png')
+				self.get_object('queue').getListItem(index).setIconImage(icon)
 			
 			'''import platform
 			hostname = platform.node()
@@ -66,7 +110,7 @@ def view_queue():
 			'''
 			
 			self.create_button('close', 'Close')
-			self.add_object("close", 9, 1)
+			self.add_object("close", 9, 2)
 			self.set_object_event('action', 'close', self.close)
 			
 	queue = QueueWindow('%s Version: %s' % (ADDON_NAME, VERSION))
